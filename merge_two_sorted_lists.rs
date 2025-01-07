d23accb5b108170e7d11e4be59000404dcb6014b
Cedric Jiang <jiangcj@pathsec.org>
2025-01-07 18:59:28 -0500

    Update merge two sorted list solution

--- merge_two_sorted_lists.rs
+++ merge_two_sorted_lists.rs
@@ -22,30 +22,25 @@ impl Solution {
         let mut list1 = list1;
         let mut list2 = list2;
 
-        let mut head = Some(Box::new(ListNode::new(0)));
+        let mut head = ListNode::new(0);
         let mut curr = &mut head;
 
-        // do the merge iteratively, a bit harder than recursion
-        while list1.is_some() && list2.is_some() {
-            if (list1.as_ref()?.val < list2.as_ref()?.val) {
+        while let (Some(l1), Some(l2)) = (&list1, &list2) {
+            if l1.val < l2.val {
                 let tmp = list1.as_mut()?.next.take();
-                curr.as_mut()?.next = list1;
+                curr.next = list1;
                 list1 = tmp;
             } else {
                 let tmp = list2.as_mut()?.next.take();
-                curr.as_mut()?.next = list2;
+                curr.next = list2;
                 list2 = tmp;
             }
-            curr = &mut curr.as_mut()?.next;
+
+            curr = curr.next.as_mut()?;
         }
 
-        if list1.is_some() {
-            curr.as_mut()?.next = list1;
-        } else {
-            // list2 may be None, but it's fine - just no-op
-            curr.as_mut()?.next = list2;
-        }
+        curr.next = list1.or(list2);
 
-        head?.next
+        head.next
     }
 }
