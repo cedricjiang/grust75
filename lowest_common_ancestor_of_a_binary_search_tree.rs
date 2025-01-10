736e2e3e434ccdd44fc379cc18dd235e9a90f4e3
Cedric Jiang <jiangcj@pathsec.org>
2025-01-10 01:57:42 -0500

    Update lowest common ancestor of a binary search tree solution

--- lowest_common_ancestor_of_a_binary_search_tree.rs
+++ lowest_common_ancestor_of_a_binary_search_tree.rs
@@ -18,7 +18,6 @@
 // }
 use std::cell::RefCell;
 use std::rc::Rc;
-
 impl Solution {
     pub fn lowest_common_ancestor(
         root: Option<Rc<RefCell<TreeNode>>>,
@@ -26,15 +25,15 @@ impl Solution {
         q: Option<Rc<RefCell<TreeNode>>>,
     ) -> Option<Rc<RefCell<TreeNode>>> {
         // safe to unwrap, they cannot be None, same for root later
-        let p = p.unwrap().borrow().val;
-        let q = q.unwrap().borrow().val;
+        let p = p?.borrow().val;
+        let q = q?.borrow().val;
 
         let mut result = root;
 
         loop {
             let tmp;
             {
-                let mut node = result.as_ref().unwrap().borrow_mut();
+                let mut node = result.as_ref()?.borrow_mut();
                 if p < node.val && q < node.val {
                     tmp = node.left.take();
                 } else if p > node.val && q > node.val {
